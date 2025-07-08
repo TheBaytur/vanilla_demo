55f71f5c5b3d5abcef1c0ecfecad89196ca4318c
TheBaytur <bayturcodding@gmail.com>
2025-07-08 21:56:54 +0800

    Vanilla demo

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: MyHomePage(),
@@ -24,64 +23,71 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-  class _MyHomePageState extends State<MyHomePage> {
-    int _rating = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  int _rating = 0;
 
   @override
   Widget build(BuildContext context) {
     double _size = 50;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Vanilla Demo'),
-      ),
+      appBar: AppBar(title: Text('Vanilla Demo')),
       body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
               child: IconButton(
-                icon: (_rating >= 1)
-                    ? Icon(Icons.star, size: _size, color: Colors.amber)
-                    : Icon(Icons.star_border, size: _size, color: Colors.grey),
-              onPressed: () {
-                setState(() {
-                  _rating = 1;
+                icon:
+                    (_rating >= 1)
+                        ? Icon(Icons.star, size: _size, color: Colors.amber)
+                        : Icon(
+                          Icons.star_border,
+                          size: _size,
+                          color: Colors.grey,
+                        ),
+                onPressed: () {
+                  setState(() {
+                    _rating = 1;
+                  });
                 },
-                );
-              },
               ),
             ),
             Container(
               child: IconButton(
-                icon: (_rating >= 2)
-                    ? Icon(Icons.star, size: _size, color: Colors.amber)
-                    : Icon(Icons.star_border, size: _size, color: Colors.grey),
-              onPressed: () {
-                setState(() { 
-                  _rating = 2;
+                icon:
+                    (_rating >= 2)
+                        ? Icon(Icons.star, size: _size, color: Colors.amber)
+                        : Icon(
+                          Icons.star_border,
+                          size: _size,
+                          color: Colors.grey,
+                        ),
+                onPressed: () {
+                  setState(() {
+                    _rating = 2;
+                  });
                 },
-                );
-              },
-              ),    
+              ),
             ),
             Container(
               child: IconButton(
-                icon: (_rating >= 3)
-                    ? Icon(Icons.star, size: _size, color: Colors.amber)
-                    : Icon(Icons.star_border, size: _size, color: Colors.grey),
-              onPressed: () {
-                setState(() { 
-                  _rating = 3;
+                icon:
+                    (_rating >= 3)
+                        ? Icon(Icons.star, size: _size, color: Colors.amber)
+                        : Icon(
+                          Icons.star_border,
+                          size: _size,
+                          color: Colors.grey,
+                        ),
+                onPressed: () {
+                  setState(() {
+                    _rating = 3;
+                  });
                 },
-                );
-              },
-              ),    
-              
-            )
+              ),
+            ),
           ],
-          
-          
         ),
       ),
     );
